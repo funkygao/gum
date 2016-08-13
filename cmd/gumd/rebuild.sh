73f294a1f88074f954915b61d4852e1e79681560
Funky Gao <funky.gao@gmail.com>
2016-08-13 10:00:57 +0800

    keep default.go controller

--- cmd/gumd/rebuild.sh
+++ cmd/gumd/rebuild.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+mv controllers/default.go .default.go
 rm -f models/* controllers/*
+mv .default.go controllers/default.go
 
 bee generate model bookmark -fields="id:auto,user:string,uri:string:255,title:text,description:text,private:bool,comment_cnt:int,hit_cnt:int,pin:bool,ctime:datetime,mtime:datetime"
 bee generate model tag -fields="bookmark_id:int64,tag:string:128,ctime:datetime"
