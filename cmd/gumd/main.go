01d38b47282cd617d96adbc3c08574249ebf7423
Funky Gao <funky.gao@gmail.com>
2016-08-13 10:08:23 +0800

    connect to mysql

--- cmd/gumd/main.go
+++ cmd/gumd/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	_ "github.com/funkygao/gum/cmd/gumd/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/funkygao/gum/cmd/gumd/routers"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func init() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", "root:@/bookmark?charset=utf8")
+}
 
 func main() {
 	beego.Run()
 }
-
