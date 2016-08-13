4b530d11a5209c9e60ad77338cc138f6230209a0
Funky Gao <funky.gao@gmail.com>
2016-08-13 09:53:13 +0800

    add feed feature

--- cmd/gumd/rebuild.sh
+++ cmd/gumd/rebuild.sh
@@ -5,7 +5,9 @@ rm -f models/* controllers/*
 bee generate model bookmark -fields="id:auto,user:string,uri:string:255,title:text,description:text,private:bool,comment_cnt:int,hit_cnt:int,pin:bool,ctime:datetime,mtime:datetime"
 bee generate model tag -fields="bookmark_id:int64,tag:string:128,ctime:datetime"
 bee generate model comment -fields="bookmark_id:int64,body:text,ctime:datetime"
+bee generate model feed -fields="id:int64,body:text,ctime:datetime"
 
 bee generate controller bookmark
 bee generate controller comment
 bee generate controller tag
+bee generate controller feed
