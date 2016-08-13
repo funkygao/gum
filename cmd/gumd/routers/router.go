57501b9391696f3042cdae868e26399c6e07d423
Funky Gao <funky.gao@gmail.com>
2016-08-13 10:23:12 +0800

    the routing is tested ok

--- cmd/gumd/routers/router.go
+++ cmd/gumd/routers/router.go
@@ -1,10 +1,17 @@
 package routers
 
 import (
-	"github.com/funkygao/gum/cmd/gumd/controllers"
 	"github.com/astaxie/beego"
+	"github.com/funkygao/gum/cmd/gumd/controllers"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+
+	ns := beego.NewNamespace("/v1",
+		beego.NSNamespace("/bookmarks", beego.NSInclude(&controllers.BookmarkController{})),
+		beego.NSNamespace("/comments", beego.NSInclude(&controllers.CommentController{})),
+		beego.NSNamespace("/tags", beego.NSInclude(&controllers.TagController{})),
+	)
+	beego.AddNamespace(ns)
 }
