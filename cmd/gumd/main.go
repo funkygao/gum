3431d1817eb9bc061572d139dcc80b0407ff6d16
Funky Gao <funky.gao@gmail.com>
2016-08-15 08:22:32 +0800

    prepare img upload dir before startup

--- cmd/gumd/main.go
+++ cmd/gumd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/funkygao/gum/cmd/gumd/routers"
@@ -12,6 +14,10 @@ func init() {
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 	orm.RegisterDataBase("default", "mysql", "root:@/bookmark?charset=utf8")
 	orm.Debug = true // TODO
+
+	if err := os.MkdirAll(beego.AppConfig.String("snapshot"), 0755); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
